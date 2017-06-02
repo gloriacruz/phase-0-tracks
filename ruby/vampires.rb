e5a48b2749e84e47f66dcf34525fcb67308e632b
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-01 18:16:28 -0700

    Detection logic for vampire status

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,9 +6,66 @@ def load_employee_data
   puts "What year were you born?"
   year = gets.to_i
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-  want_garlic = gets.chomp
+  if gets.chomp == "y"
+    eats_garlic = true
+  else
+    eats_garlic = false
+  end
   puts "Would you like to enroll in the company’s health insurance? (y/n)"
-  health_insurance = gets.chomp
+  if gets.chomp == "y"
+    health_insurance = true
+  else
+    health_insurance = false
+  end
+
+  puts "Employee Name: #{name}"
+  puts "Age: #{age}"
+  puts "Birth year: #{year}"
+  puts "Eats garlic: #{eats_garlic}"
+  puts "Wants health insurance: #{health_insurance}"
+  puts "Vampire status: " + check_vampire_status(name,age,year,eats_garlic,health_insurance)
+
+end
+
+def calculate_age(year)
+  # Assuming he/she was born A.D.
+  calculated_age = 2017 - year
+end
+
+def check_vampire_status(name,age,year,eats_garlic,health_insurance)
+
+  vampire_status = nil
+
+  calculated_age = calculate_age(year)
+
+  if age == calculated_age
+    correct_age = true
+  else
+    correct_age = false
+  end
+
+  if correct_age && (eats_garlic || health_insurance)
+    vampire_status = "Probably not a vampire"
+  end
+
+  if !correct_age && (!eats_garlic || !health_insurance)
+    vampire_status = "Probably a vampire"
+  end
+
+  if !correct_age  && !eats_garlic && !health_insurance
+    vampire_status = "Almost certainly a vampire"
+  end
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    vampire_status = "Definitely a vampire"
+  end
+
+  if vampire_status == nil
+    vampire_status = "Results inconclusive"
+  end
+
+  return vampire_status
+
 end
 
 load_employee_data
