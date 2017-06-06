dbfe3894768ff7519fcfc2e09fc8c8abcc118aee
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-06 00:23:09 -0700

    Adding pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+# Pseudocode
+# Ask user to enter the details of the client in the following order:
+# Ask for client's name
+# Ask for client's birthdate (dd-mm-yyyy)
+# Ask if client has a spouse or partner (true/false)
+# Ask for number of children
+# Ask for decor theme
+# Ask for client's city
+# Format and print information entered by user
+# Ask user what information needs updating
+# If answer is 'none'
+#     Skip updates
+# Otherwise
+#     Update corresponding value
+# Print lastest version of information
+
