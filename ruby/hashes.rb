1b5ff88e6153ca4bf7963e11de3e1b98e32410b4
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-06 01:10:05 -0700

    Added formatting and partner data type condition

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -25,8 +25,13 @@ puts "Enter client's name"
 design_client[:name] = gets.chomp
 puts "Enter client's birthdate (mm-dd-yyyy)"
 design_client[:birthdate] = gets.chomp
-puts "Does the client have a spouse or partner (true/false)"
-design_client[:partner] = gets.chomp
+puts "Does the client have a spouse or partner? (yes/no)"
+partner = gets.chomp
+If partner == "yes"
+  design_client[:partner] = true
+else
+  design_client[:partner] = false
+end
 puts "How many children does the client have?"
 design_client[:num_children] = gets.chomp
 puts "What's the decor theme?"
@@ -39,15 +44,19 @@ puts "How many bathrooms in the house?"
 design_client[:num_bathrooms] = gets.chomp
 puts "What the budget for the design project?"
 design_client[:budget] = gets.chomp
+puts "-------------------"
 puts design_client
-puts "What information do you need to update? (enter key or type 'none' to skip)"
+puts "-------------------"
+puts "What information do you need to update? (enter key name or type 'none' to skip)"
 update_key = gets.chomp
 if update_key != "none"
   puts "Enter new value for #{update_key}"
   new_value = gets.chomp
   design_client[:update_key] = new_value
 end
+puts "-------------------"
 puts design_client
+puts "-------------------"
 
 
 
