e3de05e2ae5ba7c27e457e726a9c3f9420c37873
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-07 22:56:31 -0700

    Added pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+# Pseudocode:
+# Take a spy's real name
+# Make real name lowercase
+# Convert the string into an array
+# For each letter in the array
+#   If it's vowel
+#     call method that returns next vowel
+#   Otherwise
+#     call method that returns next consonant
+#   Add/push new vowel or new consonat to new array
+# Join back the final array into a string
+# Make first letter in first and last name uppercase
+
+
