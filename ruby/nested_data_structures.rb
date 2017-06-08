d50da506ab1565afb3c994b440090d412fbf1ff0
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-07 18:21:47 -0700

    Added nested data structure for aquarium map

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,67 @@
+mty_aquarium_map = {
+  first_floor: {
+    oceans_edge: {
+      name: 'The oceans edge',
+      indoor_sections: [
+        'Kelp Forest',
+        'Sea Otters',
+        'Touch Pools',
+        'Aviary',
+        'Octopus',
+        'Store'
+        ],
+      outdoor_sections: [
+        'Great Tide Pool',
+        'Wildlife Viewing Station'
+        ],
+      cafeteria: {
+        seats: 20,
+        cashiers: 3,
+        food_stations: 3
+      },
+      bathrooms: 2,
+      elevators: 2
+    },
+    special_exhibit: {
+      name: 'The Open Sea (Special Exhibition)',
+      indoor_sections: [
+        'Tentacles',
+        'Vanishing Wildlife',
+        'Viva Baja!'
+        ],
+      outdoor_sections: [],
+      bathrooms: 1,
+      elevators: 1
+    }
+  },
+  second_floor: {
+    open_sea: {
+      name: 'The Open Sea',
+      indoor_sections: [
+        'Jellies',
+        'The Open Sea',
+        'Seabirds',
+        'Ocean Travelers'
+        ],
+      outdoor_sections: [
+        'Viewing Area'
+        ],
+      bathrooms: 1,
+      elevators: 1
+    },
+    splash_zone: {
+      name: 'Splash Zone',
+      indoor_sections: [
+        'Penguins',
+        'Touch Pools',
+        'Play Area',
+        'Splash Zone'
+        ],
+      outdoor_sections: [
+        'Viewing Area'
+        ],
+      bathrooms: 2,
+      elevators: 1
+    }
+  }
+}
