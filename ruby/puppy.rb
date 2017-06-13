73777cbbc48305a8d372f94fd57ecbb071f60122
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-12 18:16:25 -0700

    Design a class for Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# PUPPY CLASS-----------
+# Sparky the puppy
+
+# CHARACTERISTICS-----------
+# Name: Sparky
+# Color: Gray and white
+# Age: 5 months
+# Breed: Schnauzer
+# Friendliness Level: 10
+
+# BEHAVIOUR-----------
+# Eat
+# Play
+# Walk
+# Chew
