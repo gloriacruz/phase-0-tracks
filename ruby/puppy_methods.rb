36e367f05f02c9bcbad731fecd71eb0423d7546b
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-13 12:28:40 -0700

    Adding instance methods and driver code to test them for Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(i)
+  	i.times { puts "Woof! "}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dog_years = human_years * 7
+  	puts dog_years
+  end
+
+  def hi5
+  	puts "Give paw back"
+  end
+
 end
+
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+fido.dog_years(3)
+fido.hi5
+
 
