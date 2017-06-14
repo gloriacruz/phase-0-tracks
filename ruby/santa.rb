8964d791fde4800dc90c81bf8fdbe907501c7b47
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-14 14:09:35 -0700

    Added attributes for Santa class for Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,12 +8,25 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
-  def initialize
-    puts "Initializing Santa instance ..."
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+
+    puts "Initializing Santa instance: #{@gender} and #{ethnicity}..."
   end
 
 end
 
-my_santa = Santa.new
-my_santa.speak
-my_santa.eat_milk_and_cookies("gingerbread")
+# D R I V E R  C O D E - RELEASE 0
+# my_santa = Santa.new
+# my_santa.speak
+# my_santa.eat_milk_and_cookies("gingerbread")
+
+# D R I V E R  C O D E - RELEASE 1
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# For every gender, picks a random ethnicity
+genders.each { |gender| santas << Santa.new(gender,ethnicities.sample) }
