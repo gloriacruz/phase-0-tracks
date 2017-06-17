c90d9b29e690d27450c55ace87a0f1bcc85c3333
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-16 19:11:33 -0700

    Adding first version of the rspecs and code

--- ruby/word_guessing/word_guessing.rb
+++ ruby/word_guessing/word_guessing.rb
@@ -6,19 +6,114 @@
 # -Create an empty ARRAY for keeping track of the repeated guesses.
 # -Keep track of the guesses available. The total available guesses are the number of letters in the secret word.
 # -WHILE available guesses are greater then zero, GET a guess from User2:
-#  (Note: The User2 can try to guess the whole word at any time or just one letter in the word.)
+#  (NoTe: The User2 can try to guess the whole word at any time or just one letter in the word.)
 #     Check IF input is already in the previous guesses ARRAY:
 #        PRINT "Duplicated attemp!" and continue with the rest of the program.
 #     ELSE
 #        Deduct an available guess and continue with the rest of the program.
+#        PUSH word or letter to the previous guesses ARRAY.
 #     IF input's lenght is more than 1 character long:
 #        Compare input against the whole word. IF whole word matches:
 #           PRINT "Congratulations! You've guessed the word" and EXIT the program.
 #        ELSE, PRINT "Wrong attempt!" and the current state of the word.
 #     ELSE
 #        See IF the letter provided is INCLUDED in the word:
-#           IF true AND the secret word is not complete yet, PRINT the current state of the word.
+#           IF true AND the secret word is not complete yet, UPDATE and PRINT the current state of the word.
 #           ELSE if this is true AND the secret word is complete, PRINT "Congratulations! You've guessed the word" and EXIT the program.
 #        ELSE, PRINT "Wrong attempt!" and the current state of the word.
-#     IF
 # -After all attempts are over and the word was not guessed PRINT "Sorry you couldn't guess the word! It was " and concatenate secret word.
+
+class Word_Guessing
+  attr_accessor :prev_guesses, :current_state
+  attr_reader :secret_word, :available_guesses, :is_over
+
+  def initialize(secret_word)
+    puts "Initializing game for new secret word..."
+    @secret_word = secret_word
+    @current_state = ""
+    secret_word.length.times { @current_state << "_" }
+    @prev_guesses = []
+    @available_guesses = secret_word.length
+    @is_over = false
+  end
+
+  def deduct_available_guesses
+    @available_guesses -= 1
+  end
+
+  def is_repeated_guess(guess)
+    @prev_guesses.include? guess
+  end
+
+  def is_word_complete
+    !@current_state.include? "_"
+  end
+
+  def update_current_state(guess)
+    start = 0
+    while @secret_word.index(guess, start) != nil
+      @current_state[@secret_word.index(guess,start)] = guess
+      start = @secret_word.index(guess,start) + 1
+    end
+    @current_state
+  end
+
+  def add_to_previous_guesses(guess)
+    prev_guesses << guess
+  end
+
+  def try_guess(guess)
+    if guess.length > 1
+      if @secret_word == guess
+          @is_over = true
+      else
+        false
+      end
+    else
+      if @secret_word.include? guess
+        update_current_state(guess)
+        if !is_word_complete()
+          false
+        else
+          @is_over = true
+        end
+      else
+        false
+      end
+    end
+  end
+
+end
+
+# DRIVER CODE
+# User Interface: driver code will handle input and output as far as the user is concerned
+
+puts "Welcome to the word guessing game!"
+puts "User1: What's the secret word?"
+user1_input = gets.chomp
+game = Word_Guessing.new(user1_input)
+while game.available_guesses > 0 && !game.is_over
+  puts "--------------------------------------------------"
+  puts "Secret word (current state): #{game.current_state}"
+  puts "User2: Can you guess the secret word? Type the word or guess a letter in the word"
+  user2_input = gets.chomp
+  if game.is_repeated_guess(user2_input)
+    puts "Duplicated attemp!"
+  else
+    game.deduct_available_guesses
+    game.add_to_previous_guesses(user2_input)
+  end
+
+  if game.try_guess(user2_input)
+    puts "Congratulations! You've guessed the word: #{game.secret_word}"
+  else
+    puts "Secret word (current state): #{game.current_state}"
+  end
+end
+
+if !game.is_word_complete && !game.is_over
+  puts "Sorry you couldn't guess the word! The secret word was: #{game.secret_word}"
+end
+
+
+
---
+++ ruby/word_guessing/word_guessing_spec.rb
@@ -0,0 +1,69 @@
+require_relative 'word_guessing'
+
+describe Word_Guessing do
+
+let(:game) { Word_Guessing.new('parrot') }
+
+  it 'reduce the number of available guesses by 1' do
+    game.available_guesses = 5
+    expect(game.deduct_available_guesses).to eq 4
+  end
+
+  it 'it is not a repeated guess' do
+    game.prev_guesses = ['a','b','c']
+    user_input = 'p'
+    expect(game.is_repeated_guess(user_input)).to eq false
+  end
+
+  it 'it is a repeated guess' do
+    game.prev_guesses = ['a','b','c']
+    user_input = 'a'
+    expect(game.is_repeated_guess(user_input)).to eq true
+  end
+
+  it 'the secret word is not complete' do
+    game.current_state = 'parro_'
+    expect(game.is_word_complete).to eq false
+  end
+
+  it 'the secret word is complete' do
+    game.current_state = 'parrot'
+    expect(game.is_word_complete).to eq true
+  end
+
+  it 'update current state' do
+    user_input = 'r'
+    expect(game.update_current_state(user_input)).to eq '__rr__'
+  end
+
+  it 'can add to previous guesses array' do
+    game.prev_guesses = ['a','b','c']
+    user_input = 'r'
+    expect(game.add_to_previous_guesses(user_input)).to eq (['a','b','c','r'])
+  end
+
+  it 'user guesses the whole word' do
+    user_input = 'parrot'
+    expect(game.try_guess(user_input)).to eq true
+  end
+
+  it 'user did not guess the whole word' do
+    user_input = 'carrot'
+    expect(game.play(user_input)).to eq false
+  end
+
+  it 'user did not guess a letter in the word' do
+    user_input = 'u'
+    expect(game.play(user_input)).to eq false
+  end
+
+  it 'user did guess a letter in the word' do
+    user_input = 'r'
+    expect(game.play(user_input)).to eq true
+  end
+
+end
+
+
+#test no word
+#test no guess or empty guess
