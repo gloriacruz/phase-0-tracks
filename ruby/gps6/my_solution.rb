be5d5dc56795efa0a5286f1b6eb1bbb085d19d5f
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-20 13:27:15 -0700

    Refactored predicted_deaths and speed_of_spread for release 7

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -29,17 +29,18 @@ class VirusPredictor
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      death_var = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      death_var = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      death_var = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      death_var = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      death_var = 0.05
     end
 
+    number_of_deaths = (@population * death_var).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -48,18 +49,17 @@ class VirusPredictor
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
