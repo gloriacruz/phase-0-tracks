b959ffeab66798e5bfe181aa01216005c7ca2279
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-25 18:09:04 -0700

    Adding function and minor change to pseudocode

--- js/algos.js
+++ js/algos.js
@@ -5,4 +5,17 @@
 //   -FOR each phrase compare the current phrase length with the maximum length variable
 //   -IF the current phrase is longer, update the maximum length variable and the array position variable
 //   -ELSE move to the next phrase until the whole array is analized
-// -Return the array position where the longest phrase was found
+// -Return the phrase in the position where the longest phrase was found
+
+function longest_phrase(array){
+  var max_length = 0;
+  var position = 0;
+
+  for(var i=0; i<array.length; i++){
+    if(max_length < array[i].length){
+      max_length = array[i].length;
+      position = i;
+    }
+  }
+  return array[position];
+}
