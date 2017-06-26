09c159b1e2640fbcb33662effc6e18a7968b838b
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-25 17:40:09 -0700

    Adding pseudocode for Release o

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// RELEASE 0 - Pseudocode
+// -Initialize a maximum length variable to zero
+// -Initialize a maximum length array position variable
+// -LOOP through the given array starting from position 0
+//   -FOR each phrase compare the current phrase length with the maximum length variable
+//   -IF the current phrase is longer, update the maximum length variable and the array position variable
+//   -ELSE move to the next phrase until the whole array is analized
+// -Return the array position where the longest phrase was found
