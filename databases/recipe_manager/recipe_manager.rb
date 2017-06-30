0704adad50b2117e4c9f730813dc8c46c8a5609e
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-30 16:42:31 -0700

    Added intro, queries and main program file to the project

---
+++ databases/recipe_manager/recipe_manager.rb
@@ -0,0 +1,15 @@
+def menu()
+  puts 'Welcome to My Recipe Manager!'
+  puts 'Options:'
+  puts '---------------------------------'
+  puts '1. Add new recipe'
+  puts '2. Update recipe'
+  puts '3. Delete recipe'
+  puts '4. Search recipes'
+  puts '5. Select recipe randomly'
+  puts '6. See all recipes'
+  puts '---------------------------------'
+  puts 'Type option number'
+end
+
+menu
