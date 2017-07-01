51118c66073a8dcafda8b6a7b3ba93fe7a308aa3
Gloria A. Cruz <ally.cruz@gmail.com>
2017-06-30 17:02:57 -0700

    Adding option selection feature and module file

---
+++ databases/recipe_manager/crud.rb
@@ -0,0 +1,26 @@
+module Crud
+  def self.add_new_recipe()
+    puts 'Add new recipe feature coming soon...'
+  end
+
+  def self.update_recipe()
+    puts 'Update recipe feature coming soon...'
+  end
+
+  def self.delete_recipe()
+    puts 'Delete recipe feature coming soon...'
+  end
+
+  def self.search_recipe()
+    puts 'Search recipe feature coming soon...'
+  end
+
+  def self.ramdom_recipe()
+    puts 'Ramdom recipe feature coming soon...'
+  end
+
+  def self.see_all_recipe()
+    puts 'See all recipes feature coming soon...'
+  end
+
+end
--- databases/recipe_manager/recipe_manager.rb
+++ databases/recipe_manager/recipe_manager.rb
@@ -1,3 +1,5 @@
+require_relative 'crud'
+
 def menu()
   puts 'Welcome to My Recipe Manager!'
   puts 'Options:'
@@ -10,6 +12,23 @@ def menu()
   puts '6. See all recipes'
   puts '---------------------------------'
   puts 'Type option number'
+  option = gets.chomp
+  case option
+    when '1'
+      Crud.add_new_recipe()
+    when '2'
+      Crud.update_recipe()
+    when '3'
+      Crud.delete_recipe()
+    when '4'
+      Crud.search_recipe()
+    when '5'
+      Crud.ramdom_recipe()
+    when '6'
+      Crud.see_all_recipe()
+    else
+      puts 'Invalid option'
+  end
 end
 
 menu
