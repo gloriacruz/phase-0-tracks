2b1f3a04d7d6cd58a2953996d3a450464e1b6caf
Gloria A. Cruz <ally.cruz@gmail.com>
2017-07-12 12:17:36 -0700

    Added comment and fixed line of code.

--- web_dev/client_side_js/client_script.js
+++ web_dev/client_side_js/client_script.js
@@ -1,4 +1,5 @@
 // RELEASE 1
+// Change the style of a element with javascript
 var p1 = document.getElementById("p1");
 p1.style.fontWeight = "bold";
 p1.style.background = "pink";
@@ -9,7 +10,6 @@ var newHeading = document.createElement('h1');
 var h1Text= document.createTextNode("This is a new header text!");
 newHeading.appendChild(h1Text);
 var firstHeading = document.getElementById('header1');
-firstHeading.appendChild(newHeading);
 var parent = firstHeading.parentNode;
 parent.insertBefore(newHeading, firstHeading);
 
