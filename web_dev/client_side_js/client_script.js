2ad654fc2fa47cd709299684aee6bed1b0b70116
Gloria A. Cruz <ally.cruz@gmail.com>
2017-07-12 11:47:50 -0700

    Adding event listener on click.

--- web_dev/client_side_js/client_script.js
+++ web_dev/client_side_js/client_script.js
@@ -1,10 +1,10 @@
-
-console.log("Hello");
+// RELEASE 1
 var p1 = document.getElementById("p1");
 p1.style.fontWeight = "bold";
 p1.style.background = "pink";
 
-
+// RELEASE 1
+// Adding a new HTML element to the page
 var newHeading = document.createElement('h1');
 var h1Text= document.createTextNode("This is a new header text!");
 newHeading.appendChild(h1Text);
@@ -12,3 +12,12 @@ var firstHeading = document.getElementById('header1');
 firstHeading.appendChild(newHeading);
 var parent = firstHeading.parentNode;
 parent.insertBefore(newHeading, firstHeading);
+
+// RELEASE 2
+// Adding a click event listener
+firstHeading.addEventListener("click",clickTitle);
+
+function clickTitle(event){
+  event.target.style.background = "green";
+}
+
